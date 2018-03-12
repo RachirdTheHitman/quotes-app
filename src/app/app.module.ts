4fbbd28e7691aa851fcfecfffb54de2a87f40bc4
YiZhang <hyacinthzy1@gmail.com>
2018-03-13 10:15:08 +1100

    card completed and app service built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {LibraryPage} from "../pages/library/library";
 import {QuotesPage} from "../pages/quotes/quotes";
 import {SettingsPage} from "../pages/settings/settings";
 import {TabsPage} from "../pages/tabs/tabs";
+import {QuotesService} from "../services/quotes";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import {TabsPage} from "../pages/tabs/tabs";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    QuotesService
   ]
 })
 export class AppModule {}
--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {NavParams} from "ionic-angular";
+import {AlertController, NavParams} from "ionic-angular";
 import {Quote} from "../../data/quote.interface";
+import {QuotesService} from "../../services/quotes";
 
 @Component({
   selector: 'page-quotes',
@@ -9,7 +10,10 @@ import {Quote} from "../../data/quote.interface";
 export class QuotesPage implements OnInit{
   quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(private navParams: NavParams) {}
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController,
+    private quotesService: QuotesService) {}
 
   // ionViewDidLoad() {
   //   this.quoteGroup = this.navParams.data;
@@ -20,7 +24,29 @@ export class QuotesPage implements OnInit{
       this.quoteGroup = this.navParams.data;
   }
 
-  onAddToFavorite(quote) {
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            // console.log('Ok');
+            this.quotesService.addQuoteToFavorites(selectedQuote);
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          // role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!');
+          }
+        }
+      ]
+    });
 
+    alert.present();
   }
 }
